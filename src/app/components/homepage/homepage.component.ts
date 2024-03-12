801bde68151e6c3acce15fa5dbad1011ca807706
Kaique de Miranda <kbdemiranda@hotmail.com>
2024-03-12 16:22:58 -0300

    Finalizado o layout da homepage

---
+++ src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.scss'
+})
+export class HomepageComponent {
+
+}
